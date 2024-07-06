bc20196748e1587ee17ca43bb3c72ea692c31dd6
eko10 <eko3227@gmail.com>
2024-07-06 12:07:17 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+// pages/index.tsx
+"use client";
+
+import Chat from '../components/Chat';
+
+const IndexPage: React.FC = () => {
+    return (
+        <div>
+            <Chat />
+        </div>
+    );
+};
+
+export default IndexPage;
