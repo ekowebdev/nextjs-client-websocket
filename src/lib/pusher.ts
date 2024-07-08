ffc118b7b5fb79705c5ba900d8a25a3f0867bacb
Eko Prasetyo <134606375+ekowebdev@users.noreply.github.com>
2024-07-08 23:12:13 +0700

    Update pusher.ts

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -4,8 +4,6 @@ import Pusher from 'pusher-js';
 const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
     cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER!,
     wsHost: process.env.NEXT_PUBLIC_BACKEND_URL!,
-    wsPort: 6001,
-    disableStats: true,
   });
 
 export default pusher;
